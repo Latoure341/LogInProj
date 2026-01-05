2cfc23e4baea4460f55bface6ba3578a0e1ada94
Latoure341 <tukamothupi@gmail.com>
2026-01-05 16:29:18 +0200

    Add dotenv for environment variable management and update MongoDB connection

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,6 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
 const express = require('express');
 const mongoose = require('mongoose');
 //const ProductRoute = require('./routes/productRoute.js');
@@ -18,12 +21,16 @@ app.get('/', (req, res)=>{
     res.status(200).send('../frontend/index.html');
 })
 
-mongoose.connect('mongodb+srv://tukamothupi_db_user:05IZ6Q8ZE7hJbFcT@thedeveloper.bkgwozo.mongodb.net/BackendDB?retryWrites=true&w=majority')
+mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://tukamothupi_db_user:Eric232221@thedeveloper.bkgwozo.mongodb.net/BackendDB?retryWrites=true&w=majority')
   .then(() => {
         console.log('The database is connected!');
         app.listen(port, ()=>{
             console.log("Server is running on port " + port);
         });
+  })
+  .catch((error) => {
+        console.error('Database connection failed:', error);
+        process.exit(1);
   });
   
   
